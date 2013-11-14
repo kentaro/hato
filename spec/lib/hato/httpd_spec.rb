e363c9a58d7a0b0bb16f3f853d99d5925f1a5136
Kentaro Kuribayashi <kentarok@gmail.com>
2013-11-14 16:35:06 +0900

    Added a spec when api_key is not set

--- spec/lib/hato/httpd_spec.rb
+++ spec/lib/hato/httpd_spec.rb
@@ -6,18 +6,23 @@ require 'rack/test'
 describe Hato::Httpd do
   include Rack::Test::Methods
 
-  def app
+  let(:config) {
     config = Hato::Config.load(
       File.expand_path('../../../assets/config/test.rb', __FILE__)
     )
-    observer = Hato::Observer.new(config)
-    Hato::Httpd::App.set(:observer, observer)
-    Hato::Httpd::App.set(:api_key,  config.api_key)
-    Hato::Httpd::App.new
+  }
+
+  def app
+    @_app ||= -> {
+      observer = Hato::Observer.new(config)
+      Hato::Httpd::App.set(:observer, observer)
+      Hato::Httpd::App.set(:api_key,  config.api_key)
+      Hato::Httpd::App.new
+    }.call
   end
 
   describe 'index' do
-    it do
+    it 'should return response' do
       get '/'
       expect(last_response).to be_ok
       expect(last_response.body).to eq('Hato https://github.com/kentaro/hato')
@@ -25,16 +30,33 @@ describe Hato::Httpd do
   end
 
   describe 'notify' do
-    it 'success' do
-      post '/notify', {message: 'test', tag: 'test', api_key: 'test'}
-      expect(last_response).to be_ok
-      expect(last_response.body).to eq('{"status":"success","message":"Successfully sent the message you notified to me."}')
+    context 'success' do
+      context 'when api_key is set' do
+        it 'should be success with correct api_key' do
+          post '/notify', {message: 'test', tag: 'test', api_key: 'test'}
+          expect(last_response).to be_ok
+          expect(last_response.body).to eq('{"status":"success","message":"Successfully sent the message you notified to me."}')
+        end
+      end
+
+      context 'when api_key is not set' do
+        before { app.settings.api_key = nil            }
+        after  { app.settings.api_key = config.api_key }
+
+        it 'should be success without api_key' do
+          post '/notify', {message: 'test', tag: 'test'}
+          expect(last_response).to be_ok
+          expect(last_response.body).to eq('{"status":"success","message":"Successfully sent the message you notified to me."}')
+        end
+      end
     end
 
-    it 'error' do
-      post '/notify', {message: 'test', tag: 'test', api_key: 'wrong_key'}
-      expect(last_response).to be_forbidden
-      expect(last_response.body).to eq('{"status":"error","message":"API key is wrong. Confirm your API key setting of server/client."}')
+    context 'error' do
+      it 'should be error when api_key is wrong' do
+        post '/notify', {message: 'test', tag: 'test', api_key: 'wrong_key'}
+        expect(last_response).to be_forbidden
+        expect(last_response.body).to eq('{"status":"error","message":"API key is wrong. Confirm your API key setting of server/client."}')
+      end
     end
   end
 end
